b3a5a83555b8be36cd978765b84a228d40b80203
shivam2399 <dubey.shivam2399@gmail.com>
2024-05-24 08:41:52 +0530

    Implement Redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,35 +1,37 @@
-import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit'
 
-const counterReducer = (state =  {counter : 0}, action) => {
-    if(action.type === 'increment') {
-        return {
-            counter : state.counter + 1
-        }
+const initialState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter++
+        },
+        decrement(state) {
+            state.counter--
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.payload;
+        },
+        decrease(state, action) {
+            state.counter = state.counter - action.payload;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter
+        },
     }
-
-    if(action.type ==='increase') {
-        return {
-            counter : state.counter + action.value
-        }
-    }
-
-    if(action.type ==='decrease') {
-        return {
-            counter : state.counter - action.value
-        }
-    }
-
-    if(action.type === 'decrement') {
-        return {
-            counter: state.counter - 1
-        }
-    }
+})
 
 
 
-    return state;
-}
 
-const store = createStore(counterReducer)
+
+const store = configureStore({
+    reducer: counterSlice.reducer
+})
+
+export const counterActions = counterSlice.actions
 
 export default store;
